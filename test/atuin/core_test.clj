5c15803f62bed8b1da66529752789479d454b355
mwj8410 <mjackson@treehouseonline.com>
2016-09-04 22:14:02 -0500

    add CI config and tests

--- test/atuin/core_test.clj
+++ test/atuin/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [atuin.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest core-test
+  (testing "contains routList"
+    (is (> (count routList)))))
